25675a4a644abe5616da1d6f78bbc8c91b3154fd
Jérémy SEBAN <jeremy@seban.eu>
2017-01-17 17:54:51 +0100

    Implemented response parser mechanism and tested it. Missing fixed/ufixed

--- lib/etheruby/response_parser.rb
+++ lib/etheruby/response_parser.rb
@@ -9,35 +9,38 @@ module Etheruby
       @response = _response
     end
 
-    def treat_variable(param)
+    def treat_variable(param, arg)
       if match = TypeMatchers.is_sized_type(param)
         # Parameter is a sized type, e.g. uint256, byte32 ...
-        send("#{match[1]}_decode".to_sym, match[2].to_i)
+        send("#{match[1]}_decode".to_sym, match[2].to_i, arg)
 
       elsif match = TypeMatchers.is_dualsized_type(param)
         # Parameter is a dual sized array type, e.g. fixed16x16
-        send("#{match[1]}_decode".to_sym, match[2].to_i, match[3].to_i)
+        send("#{match[1]}_decode".to_sym, match[2].to_i, match[3].to_i, arg)
 
       elsif match = TypeMatchers.is_static_array_type(param)
         # Parameter is a staticly sized array type, e.g. uint256[24]
-        static_array_decode(match[1], match[2].to_i)
+        static_array_decode(match[1], match[2].to_i, arg)
 
       elsif match = TypeMatchers.is_dynamic_array_type(param)
         # Parameter is a dynamicaly sized array type, e.g. uint256[]
-        dynamic_array_decode(match[1])
+        dynamic_array_decode(match[1], arg)
 
       else
         # Parameter is a single-word type : string, bytes, address etc...
-        send("#{param}_decode".to_sym)
+        send("#{param}_decode".to_sym, arg)
 
       end
     end
 
     def parse
       if @returns.count == 1
-        treat_variable(@returns[0])
+        treat_variable(@returns[0], @response)[0]
       else
-        @returns.map { |type| treat_variable(type) }
+        @returns.map { |type|
+          value, @response = treat_variable(type, @response)
+          value
+        }
       end
     end
 
@@ -48,61 +51,88 @@ module Etheruby
 
     ##
     # int<X> decoding
-    def int_decode(size)
+    def int_decode(size, arg)
+      to_parse = arg[0..(size/4)-1]
+      arg = arg[(size/4)..@response.length]
+      in_int = to_parse.to_i(16)
+      in_bin = in_int.to_s(2).rjust(size, '0')
+      value = if in_bin[0] == '1'
+        -(in_bin.split("").map{ |i| i == '1' ? '0' : '1' }.join.to_i(2) + 1)
+      else
+        to_parse.to_i(16)
+      end
+      return value, arg
     end
 
     ##
     # uint<X> decoding
-    def uint_decode(size)
-      v, @response = @response[0..(size/4)].to_i(16), 
-                     @response[(size/4)..@response.length]
-      v
+    def uint_decode(size, arg)
+      return arg[0..(size/4)-1].to_i(16), arg[(size/4)..arg.length]
     end
 
     ##
     # ufixed<X> decoding
-    def ufixed_decode(size_i, size_d)
+    def ufixed_decode(size_i, size_d, arg)
+
     end
 
     ##
     # fixed<X> decoding
-    def fixed_decode(size_i, size_d)
+    def fixed_decode(size_i, size_d, arg)
+
     end
 
     ##
     # Decodes a static array
-    def static_array_decode(type, size)
+    def static_array_decode(type, size, arg)
+      return (0..size-1).map {
+        value, arg = treat_variable(type, arg)
+        value
+      }, arg
     end
 
     ##
     # Decodes a dynamic array
-    def dynamic_array_decode(type)
+    def dynamic_array_decode(type, arg)
+      size, arg = uint_decode(256, arg)
+      return static_array_decode(type, size, arg)
     end
 
     ##
     # byte<X> decoding
-    def byte_decode(size)
+    def byte_decode(size, arg)
+      return (0..size-1).map { |i|
+        value = arg[0..1].to_i(16)
+        arg = arg[2..arg.length]
+        value
+      }, arg
     end
 
     ##
     # address<X> decoding
-    def address_decode
+    def address_decode(arg)
+      return "0x#{arg[0..39]}", arg[40..arg.length]
     end
 
     ##
     # string<x> decoding (as bytes)
-    def string_decode
+    def string_decode(arg)
+      value, arg = bytes_decode(arg)
+      return value.pack('U*'), arg
     end
 
     ##
     # bytes (dynamic size) decoding
-    def bytes_decode
+    def bytes_decode(arg)
+      size, arg = uint_decode(256, arg)
+      return byte_decode(size, arg)
     end
 
     ##
     # boolean decoding (as uint8)
-    def bool_decode
-      uint_decode(8) == 1
+    def bool_decode(arg)
+      value, arg = uint_decode(8, arg)
+      return value == 1, arg
     end
 
   end
--- spec/unit/response_parser_spec.rb
+++ spec/unit/response_parser_spec.rb
@@ -2,10 +2,52 @@ require_relative '../../lib/etheruby/response_parser'
 
 describe Etheruby::ResponseParser do
 
+  it 'chains returns in response' do
+    a = [-7,-128,3]
+    expect(described_class.new([:int8,:int8,:int8],'f98003').parse).to eq(a)
+  end
+
+  it 'parses int correctly' do
+    expect(described_class.new([:int8],'f9').parse).to eq(-7)
+    expect(described_class.new([:int8],'80').parse).to eq(-128)
+    expect(described_class.new([:int8],'03').parse).to eq(3)
+  end
+
   it 'parses uint correctly' do
     expect(described_class.new([:uint8],'0f').parse).to eq(15)
     expect(described_class.new([:uint8],'10').parse).to eq(16)
     expect(described_class.new([:uint16],'1010').parse).to eq(4112)
+  end
+
+  it 'parses string correctly' do
+    data = '000000000000000000000000000000000000000000000000000000000000000668656c6c6f21'
+    expect(described_class.new([:string],data).parse).to eq('hello!')
+  end
+
+  it 'parses bytes array correctly' do
+    data = '000000000000000000000000000000000000000000000000000000000000000668656c6c6f21'
+    parsed_data = [104, 101, 108, 108, 111, 33]
+    expect(described_class.new([:bytes],data).parse).to eq(parsed_data)
+  end
+
+  it 'parses boolean correctly' do
+    expect(described_class.new([:bool],'01').parse).to eq(true)
+    expect(described_class.new([:bool],'00').parse).to eq(false)
+  end
+
+  it 'parses addresses correctly' do
+    data = '57eb1e64d972d9937c6f6f07a865e91608252c97'
+    expect(described_class.new([:address],data).parse).to eq("0x#{data}")
+  end
+
+  it 'parses static array correctly' do
+    data = '57eb1e64'
+    expect(described_class.new(['uint8[4]'],data).parse).to eq([87, 235, 30, 100])
+  end
+
+  it 'parses dynamic array correctly' do
+    data = '000000000000000000000000000000000000000000000000000000000000000457eb1e64'
+    expect(described_class.new(['uint8[]'],data).parse).to eq([87, 235, 30, 100])
   end
 
 end
